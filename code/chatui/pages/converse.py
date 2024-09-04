56b92d6976b8e8a128788a855feeabee916f2ca7
Soragi <igorsoltysik@hotmail.com>
2024-09-04 20:11:07 +0200

    Project Spec Modified & more...
    - Updated timeout_seconds in app 'jupyterlab'
    - Modified files: code/chatui/pages/converse.py

--- code/chatui/pages/converse.py
+++ code/chatui/pages/converse.py
@@ -36,7 +36,7 @@ from chatui.pages import utils
 
 _LOGGER = logging.getLogger(__name__)
 PATH = "/"
-TITLE = "Hybrid RAG: Chat UI"
+TITLE = "Igor's Awesome RAG!!!!"
 OUTPUT_TOKENS = 250
 MAX_DOCS = 5
 
